2e6a0888c6d8d18acce805780843d6ff0cb09d73
sindhupriya-madala <madala.sindhu@gmail.com>
2017-04-25 19:40:51 +0000

    test_script for testing PG and select_script to select data from table with command line arguments

---
+++ select_script.js
@@ -0,0 +1,35 @@
+const pg = require("pg");
+const settings = require("./settings");
+
+const client = new pg.Client({
+  user : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host : settings.host,
+  port : settings.port,
+  ssl : settings.ssl
+});
+var input = process.argv[2];
+
+function selectPeopleWithLastname(lastname, done) {
+  client.connect((err) => {
+    if(err) {
+     return console.log("Error in Connection : ",err);
+    }
+    client.query("SELECT * from famous_people where last_name = $1::text",[lastname],(error, result) => {
+     if(error) {
+        return console.error("error running query : ", error);
+      }
+    done(result.rows[0]);
+    //console.log(result.rows[0]);
+    client.end();
+    });
+  });
+};
+
+
+selectPeopleWithLastname(input,(row) => {
+  if(row) {
+    console.log(row);
+  }
+})
